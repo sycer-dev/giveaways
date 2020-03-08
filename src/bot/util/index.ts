230ee43eb0c0e681430b58a323b461b87d89c7ae
Carter <45381083+Fyko@users.noreply.github.com>
2020-03-08 13:12:17 -0600

    fix(shuffle): copy array to aviod mutation

--- src/bot/util/index.ts
+++ src/bot/util/index.ts
@@ -1,21 +1,22 @@
-export function shuffle<T>(arr: T[]): T[] {
-	for (let i = arr.length; i; i--) {
-		const j = Math.floor(Math.random() * i);
-		[arr[i - 1], arr[j]] = [arr[j], arr[i - 1]];
+export function shuffle<T>(data: T[]): T[] {
+	const array = data.slice();
+	for (let i = array.length; i; i--) {
+		const randomIndex = Math.floor(Math.random() * i);
+		[array[i - 1], array[randomIndex]] = [array[randomIndex], array[i - 1]];
 	}
-	return arr;
+	return array;
 }
 
-export function drawOne<T>(list: T[]): T {
-	const shuffled = shuffle(list);
+export function drawOne<T>(shuffled: T[]): T {
 	return shuffled[Math.floor(Math.random() * shuffled.length)];
 }
 
 export function draw<T>(array: T[], winners: number): T[] {
 	if (array.length <= winners) return array;
+	const shuffled = shuffle(array);
 	const draw: T[] = [];
 	while (draw.length < winners) {
-		const w = drawOne(array);
+		const w = drawOne(shuffled);
 		if (!draw.includes(w)) draw.push(w);
 	}
 	return draw;
