351b5f6f3f25dfcf62b8cc8cf831abda6b8a0389
Carter Himmel <carter@elhnet.net>
2020-07-04 23:43:34 -0600

    feat: rename metric names

--- src/bot/structures/Prometheus.ts
+++ src/bot/structures/Prometheus.ts
@@ -1,29 +1,29 @@
-import { Gauge, register, collectDefaultMetrics, Counter } from 'prom-client';
+import { Gauge, register, collectDefaultMetrics } from 'prom-client';
 
 export class Prometheus {
 	public constructor() {
-		collectDefaultMetrics({ prefix: 'giveaway_bot_' });
+		collectDefaultMetrics({ prefix: 'giveaway_bot2_' });
 	}
 
 	public readonly metrics = {
-		messageCounter: new Counter({
-			name: 'giveaway_bot_messages',
+		messageCounter: new Gauge({
+			name: 'giveaway_bot2_messages',
 			help: 'Total number of messages Giveaway Bot has seen.',
 		}),
 		userCounter: new Gauge({
-			name: 'giveaway_bot_users',
+			name: 'giveaway_bot2_users',
 			help: 'Total number of all users Giveaway Bot has seen.',
 		}),
 		guildCounter: new Gauge({
-			name: 'giveaway_bot_guilds',
+			name: 'giveaway_bot2_guilds',
 			help: 'Total number of all users Giveaway Bot has seen.',
 		}),
-		commandCounter: new Counter({
-			name: 'giveaway_bot_commands',
+		commandCounter: new Gauge({
+			name: 'giveaway_bot2_commands',
 			help: 'Total number of commands Giveaway Bot has ran.',
 		}),
-		eventCounter: new Counter({
-			name: 'giveaway_bot_gateway_events',
+		eventCounter: new Gauge({
+			name: 'giveaway_bot2_gateway_events',
 			help: 'Total number of events Giveawat Bot has recieved through the gateway.',
 		}),
 		register,
